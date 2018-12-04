3e479909dde073aef36d74056c57321abe9ed3c6
Mohit Gupta <mng170030@utdallas.edu>
2018-12-04 11:36:01 -0600

    Spacy testing

--- main.py
+++ main.py
@@ -1,14 +1,31 @@
 import nltk
 from nltk.stem import WordNetLemmatizer
-from nltk.parse.stanford import StanfordParser
-text = "THE ARCE BATTALION COMMAND HAS REPORTED THAT ABOUT 50 PEASANTS OF VARIOUS AGES HAVE BEEN KIDNAPPED BY TERRORISTS OF THE FARABUNDO MARTI NATIONAL LIBERATION FRONT [FMLN] IN SAN MIGUEL DEPARTMENT. "+\
-       " ACCORDING TO THAT GARRISON, THE MASS KIDNAPPING TOOK PLACE ON 30 DECEMBER IN SAN LUIS DE LA REINA.  THE SOURCE ADDED THAT THE TERRORISTS FORCED THE INDIVIDUALS, WHO WERE TAKEN TO AN UNKNOWN LOCATION, OUT OF THEIR RESIDENCES, PRESUMABLY TO INCORPORATE THEM AGAINST THEIR WILL INTO CLANDESTINE GROUPS."
-
-tokens = nltk.word_tokenize(text)
-lemma = list("")
-
-for a in tokens:
-    lemma1 = WordNetLemmatizer().lemmatize(a.lower())
-    lemma.append(lemma1)
+import spacy
+from spacy.lang.en import English
 
 
+nlp = spacy.load('en_core_web_sm')
+text = (u"When Sebastian Thrun started working on self-driving cars at "
+        u"Google in 2007, few people outside of the company took him "
+        u"seriously. “I can tell you very senior CEOs of major American "
+        u"car companies would shake my hand and turn away because I wasn’t "
+        u"worth talking to,” said Thrun, now the co-founder and CEO of "
+        u"online higher education startup Udacity, in an interview with "
+        u"Recode earlier this week.")
+doc = nlp(text)
+
+# Find named entities, phrases and concepts
+for entity in doc.ents:
+    print(entity.text, entity.label_)
+
+# Determine semantic similarities
+doc1 = nlp(u"my fries were super gross")
+doc2 = nlp(u"such disgusting fries")
+similarity = doc1.similarity(doc2)
+print(doc1.text, doc2.text, similarity)
+
+#Tokenizer
+tokenizer = English().Defaults.create_tokenizer(nlp)
+tokens = tokenizer(u'This is a sentence')
+assert len(tokens) == 4
+print(tokens[0])
